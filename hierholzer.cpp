c7b7271064c78a787f64a3ccdc67d86856a95465
Pedro <danielpedrosa250407@gmail.com>
2026-09-22 22:14:50 +0700

    Update hierholzer.cpp

--- hierholzer.cpp
+++ hierholzer.cpp
@@ -1,1 +1,84 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+using namespace std;
 
+struct Edge {
+    int to;
+    int id;
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<Edge>> adj(n + 1);
+    vector<int> degree(n + 1);
+
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+
+        adj[a].push_back({b, i});
+        adj[b].push_back({a, i});
+
+        degree[a]++;
+        degree[b]++;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (degree[i] % 2 != 0) {
+            cout << "IMPOSSIBLE\n";
+            return 0;
+        }
+    }
+
+    vector<bool> used(m, false);
+    vector<int> index(n + 1, 0);
+    vector<int> circuit;
+    stack<int> st;
+
+    st.push(1);
+
+    while (!st.empty()) {
+        int v = st.top();
+
+        while (index[v] < (int)adj[v].size() &&
+               used[adj[v][index[v]].id]) {
+            index[v]++;
+        }
+
+        if (index[v] == (int)adj[v].size()) {
+            circuit.push_back(v);
+            st.pop();
+        } else {
+            Edge e = adj[v][index[v]++];
+            
+            if (!used[e.id]) {
+                used[e.id] = true;
+                st.push(e.to);
+            }
+        }
+    }
+
+    if ((int)circuit.size() != m + 1) {
+        cout << "IMPOSSIBLE\n";
+        return 0;
+    }
+
+    reverse(circuit.begin(), circuit.end());
+
+    if (circuit.front() != 1 || circuit.back() != 1) {
+        cout << "IMPOSSIBLE\n";
+        return 0;
+    }
+
+    for (int v : circuit)
+        cout << v << " ";
+
+    cout << "\n";
+}
